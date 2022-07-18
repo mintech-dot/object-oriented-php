2c08760ec061394c788a5b33914866959d695506
Sobhi Raid <75437079+mintech-dot@users.noreply.github.com>
2022-07-18 09:00:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php 
 
+/* Define the class */
   class User {
 
   }
 
+/* Create an object */
   $userOne = new User();
   $userTwo = new User();
 
+/* Get tne class name using 'get_class' function */
   echo 'the class is: ' . get_class($userOne);
 
 ?>
